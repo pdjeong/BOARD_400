9cbf5b30b0c51704feacd7126fd0e5c0f7fd7e14
pdjeong <pdjeong70@gmail.com>
2025-01-04 17:06:24 +0900

    Git Connetion

---
+++ src/main/java/com/pdjeong/board/Board400Application.java
@@ -0,0 +1,13 @@
+package com.pdjeong.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board400Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board400Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdjeong/board/Board400ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdjeong.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board400ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
